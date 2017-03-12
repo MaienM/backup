d4d4cf184bf0593c25cc40e9daccea000a359967
Michon van Dooren <michon1992@gmail.com>
2017-03-12 21:01:42 +0100

    Switched to borg backup (a fork of attic that is actually maintained)
    Changed the output into an asciidoc-compatible format, for pretty
    error/status emails if desired

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o pipefail
+
 # Check arguments
 if [[ -z "$1" || ! -f "$1" ]]; then
     echo "Usage: $0 [env file] [paths...]"
@@ -7,7 +9,7 @@ if [[ -z "$1" || ! -f "$1" ]]; then
     echo "env file is a file with the following environment variables:"
     echo "  DESTINATION: the root destination folder. A subfolder in this"
     echo "    folder will be used for each of the source paths"
-    echo "  ATTIC_PASSPHRASE: the passphrase to use"
+    echo "  BORG_PASSPHRASE: the passphrase to use"
     echo "  KEEP_HOURLY: the amount of hourly backups to keep"
     echo "  KEEP_DAILY: the amount of daily backups to keep"
     echo "  KEEP_WEEKLY: the amount of weekly backups to keep"
@@ -30,42 +32,44 @@ source "$ENV_FILE"
 
 # Function to easily abort on failure
 function die() {
-    echo "==================== Results ===================="
     echo
-    echo >&2 "Action failed, aborting"
+    echo >&2 "WARNING: Action failed, aborting"
     exit 1
 }
 
-# Function that calls the attic script
-function attic() {
-    "$(realpath "${BASH_SOURCE%/*}")/attic.sh" "$@"
+# Function that calls the borg script
+function borg() {
+    "$(realpath "${BASH_SOURCE%/*}")/borg.sh" "$@" | sed 's/^/  /'
 }
 
-for bdir in "$@"; do
-    echo "==================== $bdir ===================="
+echo "= Backup report"
+echo "$(hostname)"
+echo "$(date +'%Y-%m-%d %H:%M')"
+echo
 
-    # Create new backups
-    echo "===== Backup ====="
+for bdir in "$@"; do
+    echo "== $bdir"
     echo
-    attic "$ENV_FILE" "$bdir" init /destination -e passphrase &> /dev/null \
+    borg "$ENV_FILE" "$bdir" init -e keyfile &> /dev/null \
         && echo "Initialized new repository" \
         || echo "Using existing repository"
-    attic "$ENV_FILE" "$bdir" create -s "/destination::$(date +'%Y-%m-%d_%H:%M:%S')" "/source" || die
+    echo
+
+    # Create new backups
+    echo "=== Backup"
+    echo
+    borg "$ENV_FILE" "$bdir" create -v -s "::$(date +'%Y-%m-%d_%H:%M:%S')" "/source" || die
+    echo
 
     # Cleanup old backups
-    echo "===== Cleanup ====="
+    echo "=== Cleanup"
     echo
-    attic "$ENV_FILE" "$bdir" prune -s /destination \
+    borg "$ENV_FILE" "$bdir" prune -v -s \
         --keep-hourly="$KEEP_HOURLY" \
         --keep-daily="$KEEP_DAILY" \
         --keep-weekly="$KEEP_WEEKLY" \
         --keep-monthly="$KEEP_MONTHLY" \
         --keep-yearly="$KEEP_YEARLY" \
     || die
-
     echo
 done
-
-echo "==================== Results ===================="
-echo
-echo "Backups completed!"
--- borg.sh
+++ borg.sh
@@ -8,15 +8,17 @@ if [[ -z "$1" || ! -f "$1" || -z $2 || ! -d "$2" ]]; then
     echo "  DESTINATION: the root destination folder. A subfolder in this"
     echo "    folder will be used for the backup, depending on the backed up"
     echo "    path"
-    echo "  ATTIC_PASSPHRASE: the passphrase to use"
+    echo "  BORG_PASSPHRASE: the passphrase to use"
     echo
     echo "path is the folder to backup"
     echo
-    echo "As attic will be ran inside a docker container, the paths you pass"
+    echo "As borg will be ran inside a docker container, the paths you pass"
     echo "must the ones inside of the container. These are the following:"
     echo "  /source: the source directory (passed to this command as path)"
     echo "  /destination: the destination directory (a subdirectory of"
-    echo "    \$DESTINATION based on the source path)"
+    echo "    \$DESTINATION based on the source path). You shouldn't need to"
+    echo "    ever use this, as BORG_REPO is set to this path, so not"
+    echo "    specifying the repository name should work"
     exit 1
 fi
 
@@ -27,18 +29,29 @@ shift 2
 
 # Options
 DESTINATION="${DESTINATION:-/backups}"
+OUTER_BORG_KEYS_DIR="${OUTER_BORG_KEYS_DIR:-/var/lib/borgbackup/keys}"
+OUTER_BORG_SECURITY_DIR="${OUTER_BORG_SECURITY_DIR:-/var/lib/borgbackup/security}"
+OUTER_BORG_CACHE_DIR="${OUTER_BORG_CACHE_DIR:-/var/lib/borgbackup/cache}"
 source "$ENV_FILE"
 
 # Make sure the destination directory exists
 DESTINATION="$DESTINATION/${SOURCE//\//_}"
 mkdir -p "$(dirname $DESTINATION)"
 
-# Run the attic command
+# Run the borg command
 docker run \
     --rm \
-    -h "$HOSTNAME-attic" \
+    -t \
+    -h "$HOSTNAME-borg" \
     -w / \
     -v "$SOURCE":/source \
     -v "$DESTINATION":/destination \
+    -v "$OUTER_BORG_KEYS_DIR":/borg/keys \
+    -v "$OUTER_BORG_SECURITY_DIR":/borg/security \
+    -v "$OUTER_BORG_CACHE_DIR":/borg/cache \
     --env-file "$ENV_FILE" \
-    pataquets/attic "$@"
+    -e "BORG_REPO=/destination" \
+    -e "BORG_KEYS_DIR=/borg/keys" \
+    -e "BORG_SECURITY_DIR=/borg/security" \
+    -e "BORG_CACHE_DIR=/borg/cache" \
+    maienm/borgbackup "$@"
