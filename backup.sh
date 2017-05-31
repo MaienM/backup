f76537e9a6c7d30298ca9fb617a27156c239a324
Michon van Dooren <michon1992@gmail.com>
2017-05-31 09:23:30 +0200

    Added support for setting the backup targets in the env file. Documented variable defaults

--- backup.sh
+++ backup.sh
@@ -2,19 +2,34 @@
 
 set -o pipefail
 
+DEFAULT_KEEP_HOURLY=48
+DEFAULT_KEEP_DAILY=14
+DEFAULT_KEEP_WEEKLY=8
+DEFAULT_KEEP_MONTHLY=-1
+DEFAULT_KEEP_YEARLY=-1
+
 # Check arguments
 if [[ -z "$1" || ! -f "$1" ]]; then
     echo "Usage: $0 [env file] [paths...]"
     echo
     echo "env file is a file with the following environment variables:"
-    echo "  DESTINATION: the root destination folder. A subfolder in this"
-    echo "    folder will be used for each of the source paths"
-    echo "  BORG_PASSPHRASE: the passphrase to use"
-    echo "  KEEP_HOURLY: the amount of hourly backups to keep"
-    echo "  KEEP_DAILY: the amount of daily backups to keep"
-    echo "  KEEP_WEEKLY: the amount of weekly backups to keep"
-    echo "  KEEP_MONTHLY: the amount of monthly backups to keep"
-    echo "  KEEP_YEARLY: the amount of yearly backups to keep"
+    echo "  DESTINATION: the root destination folder. A subfolder in this folder will be"
+    echo "    used for each of the source paths"
+    echo "  TARGETS: the paths to backup. Combined with the paths passed as arguments"
+    echo "  KEEP_HOURLY: the amount of hourly backups to keep [$DEFAULT_KEEP_HOURLY]"
+    echo "  KEEP_DAILY: the amount of daily backups to keep [$DEFAULT_KEEP_DAILY]"
+    echo "  KEEP_WEEKLY: the amount of weekly backups to keep [$DEFAULT_KEEP_WEEKLY]"
+    echo "  KEEP_MONTHLY: the amount of monthly backups to keep [$DEFAULT_KEEP_MONTHLY]"
+    echo "  KEEP_YEARLY: the amount of yearly backups to keep [$DEFAULT_KEEP_YEARLY]"
+    echo
+    echo "Of course, all of these can also just be set in the environment, as long as they"
+    echo "are not overridden in the env file"
+    echo
+    echo "paths are the paths to backup. Is combined with \$TARGETS"
+    echo
+    echo "This script uses borg.sh internally, with the same env file, and thus supports"
+    echo "all environment variables it does. In particular, DESTINATION and"
+    echo "BORG_PASSPHRASE are important. Please run borg.sh to see it's help information"
     exit 1
 fi
 
@@ -23,11 +38,11 @@ ENV_FILE="$1"
 shift 1
 
 # Options
-KEEP_HOURLY=${KEEP_HOURLY:-48}
-KEEP_DAILY=${KEEP_DAILY:-14}
-KEEP_WEEKLY=${KEEP_WEEKLY:-8}
-KEEP_MONTHLY=${KEEP_MONTHLY:--1}
-KEEP_YEARLY=${KEEP_YEARLY:--1}
+KEEP_HOURLY="${KEEP_HOURLY:-$DEFAULT_KEEP_HOURLY}"
+KEEP_DAILY="${KEEP_DAILY:-$DEFAULT_KEEP_DAILY}"
+KEEP_WEEKLY="${KEEP_WEEKLY:-$DEFAULT_KEEP_WEEKLY}"
+KEEP_MONTHLY="${KEEP_MONTHLY:-$DEFAULT_KEEP_MONTHLY}"
+KEEP_YEARLY="${KEEP_YEARLY:-$DEFAULT_KEEP_YEARLY}"
 source "$ENV_FILE"
 
 # Function that calls the borg script
@@ -70,7 +85,16 @@ echo
 
 declare -A statuses
 failed=0
-for bdir in "$@"; do
+for bdir in "$TARGETS" "$@"; do
+    # If this folder has already been processed, skip
+    if [[ -n $statuses["$bdir"] ]]; then
+        echo
+        echo "Folder has already been backed up this run, skipping"
+        echo
+        continue
+    fi
+
+    # Backup the directory
     process_directory "$bdir"
     statuscode=$?
 
